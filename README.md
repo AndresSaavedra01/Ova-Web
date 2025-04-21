2baf285d444ff008b2ab26d009bac00767ccb9da
andres saavedra <andres.saavedra01@uceva.edu.co>
2025-04-21 14:46:12 -0500

    Instrucciones

---
+++ README.md
@@ -0,0 +1,14 @@
+ Programa para ejecutar comandos en un maquina de AWS remotamente
+
+ para probar:
+
+ en main.c cambia los valores de:
+  const char* rutaKey = "ruta de la llavae de la maquina aws";
+  const char* rutaDirectorio = "directorio de la maquina aws donde se va ejecutar los comandos";
+  const char* ipAWS = "ip publica de la maquina";
+
+ ejecuta en consola:
+
+ gcc main.c validaciones.s  -o ejecutable
+ javac OvaWeb.java
+ java OvaWeb
--- main.c
+++ main.c
@@ -6,12 +6,16 @@
 extern int validar_comando(const char*);
 
 int main(int argc, char* argv[]) {
+    const char* rutaKey = "ruta de la llavae de la maquina aws";
+    const char* rutaDirectorio = "directorio de la maquina aws donde se va ejecutar los comandos";
+    const char* ipAWS = "ip publica de la maquina";
+
     if (argc < 2) {
         printf("Uso: %s \"comando\"\n", argv[0]);
         return 1;
     }
 
-    char* comando = argv[1];
+    const char* comando = argv[1];
 
     // Validar comando
     if (!validar_comando(comando)) {
@@ -20,12 +24,10 @@ int main(int argc, char* argv[]) {
     }
 
     // Construir y ejecutar el comando SSH
-    char full_cmd[300];
-    snprintf(
-        full_cmd, sizeof(full_cmd),
-        "ssh -i /home/andres/Descargas/keys/id_rsa.pem ubuntu@3.138.123.82 'cd ova-web && %s' 2>&1 > resultado_comando.txt",
-        comando
-    );
+    char full_cmd[512];
+    snprintf(full_cmd, sizeof(full_cmd),
+             "ssh -i %s ubuntu@%s 'cd %s && %s' > resultado_comando.txt 2>&1",
+             rutaKey, ipAWS, rutaDirectorio, comando);
 
     int res = system(full_cmd);
     printf("Comando ejecutado con código: %d\n", res);
